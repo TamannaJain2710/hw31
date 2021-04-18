2033a8b2231d2b239d7e99606f073b563a4c66df
Tamanna <thecreativefeather725@gmail.com>
2021-04-18 20:05:37 +0530

    hvj

--- sketch.js
+++ sketch.js
@@ -14,18 +14,18 @@ function setup(){
     var canvas = createCanvas(400,600);
     engine = Engine.create();
     world = engine.world;
-    drops = new drop(16, 4);
+    drops = new drop(random(50,400),random(50,400));
 }
 
 function draw(){
-    background("white")
+    background("black")
     Engine.update(engine);
     drops.display();
     for (var i =0; i<maxDrops; i++){
-        createdrops.push(new drop(random(0,400),random(0,400)))
+        createdrops.push(new drop(random(50,400),random(50,400)))
     }
-    if ((this.body.position.y > height) && (frameCount % 2 == 0) ){
-        Matter.Body.setPosition(this.body, {x:random(0,400), y:random(0,400)})
+    if ((drops.body.position.y > drops.body.height) && (frameCount % 2 == 0) ){
+        Matter.Body.setPosition(drops.body, {x:random(0,400), y:random(0,400)})
     }
     
 }   
