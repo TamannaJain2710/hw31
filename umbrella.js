45304086fc49d42c7e7bbbfa63bdb7bb9d7556c1
Tamanna <thecreativefeather725@gmail.com>
2021-04-24 12:34:05 +0530

    hvj

--- umbrella.js
+++ umbrella.js
@@ -4,10 +4,10 @@ class umbrella{
             'isStatic':true
         }
         
-        this.body = Bodies.circle(x,y,10,options)
-        this.x = x;
-        this.y = y;
-        this.radius = 10
+        this.body = Bodies.circle(x,y,100,options)
+        //this.x = x;
+        //this.y = y;
+        this.radius = 100
         this.image =  loadImage("walking_1.png")
         World.add(world, this.body);
         
@@ -17,10 +17,10 @@ class umbrella{
 
     display(){
         
-        push()
+       // push()
         imageMode(CENTER);
-        image(this.image, this.body.position.x, this.body.position.y, this.radius);                
-        pop();
+        image(this.image, this.body.position.x, this.body.position.y, 300,300);                
+      //  pop();
        
     }
 }
